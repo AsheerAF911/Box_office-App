8a9bb5a3b0fd0d636dc0eacc7ac725e6d04f2996
AsheerAF911 <alamfarooqia@gmail.com>
2023-07-06 22:02:03 +0530

    read more link added

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getShowById } from '../api/tvmaze.js';
 
 const Show = () => {
   const { showId } = useParams();
 
-  return <div>Show Items for show {showId}</div>;
+  const [showData, setShowData] = useState();
+  const [showError, setShowError] = useState();
+
+  useEffect(() => {
+    //getShowById()
+
+    async function fetchData() {
+      try {
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (error) {
+        setShowError(error);
+      }
+    }
+
+    fetchData();
+  }, [showId]);
+
+  if (showError) {
+    return <div>We have an error: {showError.message}</div>;
+  }
+
+  if (showData) {
+    return <div>We have data: {showData.name}</div>;
+  }
+
+  return <div>Data is loading....</div>;
 };
 
 export default Show;
