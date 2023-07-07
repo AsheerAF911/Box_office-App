9659b34cf444e12c7f95c16b65929ee9a50dbd78
AsheerAF911 <alamfarooqia@gmail.com>
2023-07-07 20:50:04 +0530

    Using React Query for fetching data

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,27 +1,14 @@
-import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze.js';
+import { useQuery } from '@tanstack/react-query';
 
 const Show = () => {
   const { showId } = useParams();
 
-  const [showData, setShowData] = useState();
-  const [showError, setShowError] = useState();
-
-  useEffect(() => {
-    //getShowById()
-
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (error) {
-        setShowError(error);
-      }
-    }
-
-    fetchData();
-  }, [showId]);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an error: {showError.message}</div>;
