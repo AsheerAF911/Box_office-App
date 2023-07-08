b01efa9708c8d103dd1410d3bf87d9431fc44d4e
AsheerAF911 <asheeraf007@gmail.com>
2023-07-09 01:32:45 +0530

    working on read more section

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze.js';
 import { useQuery } from '@tanstack/react-query';
+import ShowMainData from '../components/shows/ShowMainData.jsx';
+import Details from '../components/shows/Details.jsx';
 
 const Show = () => {
   const { showId } = useParams();
@@ -15,7 +17,25 @@ const Show = () => {
   }
 
   if (showData) {
-    return <div>We have data: {showData.name}</div>;
+    return (
+      <div>
+        <ShowMainData
+          image={showData.image}
+          name={showData.name}
+          rating={showData.rating}
+          summary={showData.summary}
+          genres={showData.genres}
+        />
+        <div>
+          <h1>Details</h1>
+          <Details
+            status={showData.status}
+            premiered={showData.premiered}
+            network={showData.network}
+          />
+        </div>
+      </div>
+    );
   }
 
   return <div>Data is loading....</div>;
